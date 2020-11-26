e2e037938144ceb0d1b75553798284834bf75d35
Alexander Abroskin <aleksandr.abroskin@instamart.ru>
2020-11-26 12:47:59 +0300

    remove unnecesary middleware

--- lib/pghero/engine.rb
+++ lib/pghero/engine.rb
@@ -6,12 +6,11 @@ module PgHero
       PgHero.time_zone = PgHero.config["time_zone"] if PgHero.config["time_zone"]
       # View files working with flash
       config.middleware.use ActionDispatch::Flash
-      config.middleware.use ActionDispatch::Static
 
       # We can add all of the public assets from our engine and make them
       # available to use.  This allows us to use javascripts, images, stylesheets
       # etc.
-      config.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
+      config.middleware.insert_before(::ActionDispatch::Flash, ::ActionDispatch::Static, "#{root}/public")
     end
   end
 end
